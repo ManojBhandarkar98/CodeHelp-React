8e21afb87ff2614d8a62750195d31dd36c981274
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-01 21:20:01 +0530

    Epi 01 - accessing data from component in another component using Props

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import Item from './Componants/Item';
 import ItemDate from './Componants/ItemDate';
 
 function App() {
+  const itemTwoName = "SurfExcel"
   return (
   <div>
     <Item itemName="Nirma"></Item>
     <ItemDate day='20' month='June' year='1992'></ItemDate>
+    <Item itemName={itemTwoName}></Item>    
+    <ItemDate day='22' month='August' year='1989'></ItemDate>
+    <Item itemName="555"></Item>
+    <ItemDate day='20' month='July' year='1984'></ItemDate>
     <div className="App">Hellow Jee React</div>
     </div>
   );
