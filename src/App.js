5c48bd441ec000cbc29fd67bd734cdec5b747f50
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-02 09:07:59 +0530

    Epi - 01 - parent child

--- src/App.js
+++ src/App.js
@@ -1,41 +1,44 @@
 
 import './App.css';
+import Card from './Componants/Card';
 import Item from './Componants/Item';
 import ItemDate from './Componants/ItemDate';
 
 function App() {
-  
+
   const response = [
     {
       itemName: "Nirma",
-      itemDate:"20",
-      itemMonth:"June",
-      itemYear:"1998"
+      itemDate: "20",
+      itemMonth: "June",
+      itemYear: "1998"
     },
     {
       itemName: "SurfExcel",
-      itemDate:"10",
-      itemMonth:"August",
-      itemYear:"1908"
-    },{
+      itemDate: "10",
+      itemMonth: "August",
+      itemYear: "1908"
+    }, {
       itemName: "555",
-      itemDate:"29",
-      itemMonth:"April",
-      itemYear:"1989"
+      itemDate: "29",
+      itemMonth: "April",
+      itemYear: "1989"
     }
   ]
   return (
-  <div>
-    <Item itemName={response[0].itemName}>
-      Hello jee mai hu apki first child item
-    </Item>
-    <ItemDate day={response[0].itemDate} month={response[0].itemMonth} year={response[0].itemYear}></ItemDate>
-    <Item itemName={response[1].itemName}></Item>
-    <ItemDate day={response[1].itemDate} month={response[1].itemMonth} year={response[1].itemYear}></ItemDate>
-    <Item itemName={response[2].itemName}></Item>
-    <ItemDate day={response[2].itemDate} month={response[2].itemMonth} year={response[2].itemYear}></ItemDate>
-   
-    <div className="App">Hellow Jee React</div>
+    <div>
+      <Card>
+        <Item itemName={response[0].itemName}>
+          Hello jee mai hu apki first child item
+        </Item>
+        <ItemDate day={response[0].itemDate} month={response[0].itemMonth} year={response[0].itemYear}></ItemDate>
+        <Item itemName={response[1].itemName}></Item>
+        <ItemDate day={response[1].itemDate} month={response[1].itemMonth} year={response[1].itemYear}></ItemDate>
+        <Item itemName={response[2].itemName}></Item>
+        <ItemDate day={response[2].itemDate} month={response[2].itemMonth} year={response[2].itemYear}></ItemDate>
+
+        <div className="App">Hellow Jee React</div>
+      </Card>
     </div>
   );
 }
