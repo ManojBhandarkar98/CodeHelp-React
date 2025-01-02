4865f39692f2523d1adf4928c0c6239f9b3205d3
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-02 17:59:47 +0530

    Epi - 02 -data input text, date caputured using onchange, update data using useState, change form behavior default prevent and create object, after adding data box will be empty

--- src/Componants/ProductForm.js
+++ src/Componants/ProductForm.js
@@ -1,24 +1,40 @@
+import { useState } from "react";
 import "./ProductForm.css"
-function ProductForm(){
-return (<form>
-    <div>
-    <div className="new-product_control">
-        <label>
-            Title
-        </label>
-        <input type="text"></input>
-    </div>
-    <div className="new-product_control">
-        <label>Date</label>
-        <input type="date" min='2025-01-01' max='2025-12-12'></input>
-    </div>
-    <div className="new-product_button">
-    <button type="submit">
-    Add Product
-    </button>
-    </div>
-    </div>
+function ProductForm() {
+
+    const [title, setTitle] = useState('');
+    const [date, setDate] = useState('');
+    function titleChangeHandler(event) {
+        setTitle(event.target.value)
+    }
+    function dateChangeHandler(event) {
+        setDate(event.target.value);
+    }
+
+    function submitHandler(event){
+        event.preventDefault();
+        const productData = {
+            title:title,
+            date:date
+        }
+        setTitle('');
+        setDate('');
+    }
+    return (<form onSubmit={submitHandler}>
+        <div className="new-product_controls">
+            <div className="new-product_control">
+                <label>Title</label>
+                <input type="text" onChange={titleChangeHandler}></input>
+            </div>
+            <div className="new-product_control">
+                <label>Date</label>
+                <input type="date" onChange={dateChangeHandler} min='2025-01-01' max='2025-12-12'></input>
+            </div>
+            <div className="new-product_button">
+                <button type="submit">Add Product</button>
+            </div>
+        </div>
     </form>
-);
+    );
 }
 export default ProductForm;
